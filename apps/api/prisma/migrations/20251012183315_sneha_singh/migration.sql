dd7a8a6ec46103fffb09b2b852f89b080c6228f6
luciferdk <dksinha0701@gmail.com>
2025-11-02 18:00:38 +0530

    remove backend from frontend caused issue on devlopment in backend

--- apps/api/prisma/migrations/20251012183315_sneha_singh/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `nickname` on the `Contact` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."Contact" DROP COLUMN "nickname",
-ADD COLUMN     "nickName" TEXT;
--- apps/api/src/routes/routesContact.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import  addNewContact  from '../controller/contact.controller';
-import { verifyToken } from '../utils/session';
-
-const router = express.Router();
-
-router.post('/addcontact', verifyToken, addNewContact);
-
-export default router;
